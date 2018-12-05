42688805fd3a10f2070e071f2ed603d77f657421
Yoan Ante <yoanante@gmail.com>
2018-12-05 18:03:29 -0500

    Alert bug is no funo

--- src/index.js
+++ src/index.js
@@ -1,35 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
   const board = document.getElementById('board')
   board.addEventListener("click", checkState)
+  let gameBoard = ["","","","","","","","",""]
 
   //intialize the state of the board when the DOM Loads player1 always starts therefor set var for p1Turn to true
   const player1 = "X"
-  const player2 = "0"
+  const player2 = "O"
   let p1Turn    = true
+  let won       = false
 
-  function theBoard(fill){
-    let board = ["","","","","","","",""]
+  function drawBoard(aBoard){
+    aBoard.forEach(function(piece, index){
+      console.log(piece);
+      if (piece === "X"){
+        document.querySelectorAll(`[data-id='${index+1}']`)[0].children[1].style.display ="block"
+      }
+      else if(piece === "O"){
+        document.querySelectorAll(`[data-id='${index+1}']`)[0].children[0].style.display ="block"
+      }
+    })
+    if (!won){
+      checkWinningGame()
+  }
+
   }
 
   function checkState(event){
-
     //get the text content of the Span
-    let innerSpace = event.target.dataset.token
-    console.log(event.target.dataset.token);
+    let token = event.target.dataset.token
 
     // gamePlay
-    if(p1Turn && innerSpace === ""){
+    if(p1Turn && token === ""){
       event.target.dataset.token = "X"
-      event.target.children[1].style.display = "block"
+      gameBoard[event.target.dataset.id-1] = event.target.dataset.token
       p1Turn = false
-      checkWinningGame()
-    }else if (!p1Turn && innerSpace === ""){
+      console.log("gameboard is", gameBoard)
+      console.log("----")
+      drawBoard(gameBoard)
+    }else if (!p1Turn && token === ""){
       event.target.dataset.token = "O"
-      event.target.children[0].style.display = "block"
+      gameBoard[event.target.dataset.id-1] = event.target.dataset.token
       p1Turn = true
-      checkWinningGame()
-    }else{
-      return
+      console.log("p2 check state")
+      console.log("----")
+      drawBoard(gameBoard)
     }
   }
 
@@ -38,15 +52,18 @@ document.addEventListener('DOMContentLoaded', () => {
   //[1,2,3]  [4,5,6]  [7,8,9] [1,4,7] [2,5,8] [3,6,9] [1,5,9] [3,5,7]
   function checkWinningGame(){
     console.log("in winning game");
-    let a1 = document.getElementById('1').children[0].dataset.token
-    let b2 = document.getElementById('2').children[0].dataset.token
-    let c3 = document.getElementById('3').children[0].dataset.token
-    let d4 = document.getElementById('4').children[0].dataset.token
-    let e5 = document.getElementById('5').children[0].dataset.token
-    let f6 = document.getElementById('6').children[0].dataset.token
-    let g7 = document.getElementById('7').children[0].dataset.token
-    let h8 = document.getElementById('8').children[0].dataset.token
-    let i9 = document.getElementById('9').children[0].dataset.token
+    console.log("------")
+    let a1 = gameBoard[0]
+    let b2 = gameBoard[1]
+    let c3 = gameBoard[2]
+    let d4 = gameBoard[3]
+    let e5 = gameBoard[4]
+    let f6 = gameBoard[5]
+    let g7 = gameBoard[6]
+    let h8 = gameBoard[7]
+    let i9 = gameBoard[8]
+    console.log("i9 is", i9)
+    console.log("------")
     if ((a1 === "X" && b2 === "X" && c3 === "X")||
         (d4 === "X" && e5 === "X" && f6 === "X")||
         (g7 === "X" && h8 === "X" && i9 === "X")||
@@ -59,7 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
                       player: 'Player 1'}
           endGame(game)
           return "Player 1"
-    }else if((a1 === "X" && b2 === "X" && c3 === "X")||
+    }else if((a1 === "O" && b2 === "O" && c3 === "O")||
         (d4 === "O" && e5 === "O" && f6 === "O")||
         (g7 === "O" && h8 === "O" && i9 === "O")||
         (a1 === "O" && d4 === "O" && g7 === "O")||
@@ -75,19 +92,25 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function endGame(game){
-      alert(`${game.player} wins! Taco`)
+      won = true
+      //drawBoard(game.board)
       console.log(JSON.stringify(game))
-      resetBoard()
+      //resetBoard()
+      // debugger
+      alert(`${game.player} wins! Taco`)
+
       //fetch create new game in database
     }
 
   function resetBoard(){
     Array.from(document.getElementsByClassName('interior-space')).forEach(function(interiorDivElements){
-      console.log(interiorDivElements);
       interiorDivElements.dataset.token = ""
       interiorDivElements.children[0].style.display = "none"
       interiorDivElements.children[1].style.display = "none"
     })
+    won     = false
+    p1Turn  = true
+    gameBoard = ["","","","","","","","",""]
   }
 
 });
