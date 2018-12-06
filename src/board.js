45f79dff8104fc1a5619454451a94891b82e024e
Yoan Ante <yoanante@gmail.com>
2018-12-05 20:56:47 -0500

    refactored to create Board objects

--- src/board.js
+++ src/board.js
@@ -1,11 +1,68 @@
+class Board {
+  //player1 and player2 take an "X" or an "O"
+  constructor(player1, player2){
+    this.player1 = player1
+    this.player2 = player2
+    this.p1Turn = true
+    this.won = false
+    this.gameBoard = ["","","","","","","","",""]
+  }
 
-{
-  [1,2,3],
-  [4,5,6],
-  [7,8,9],
-  [1,4,7],
-  [2,5,8],
-  [3,6,9],
-  [1,5,9],
-  [7,8,9],
+  //checks to see if a game has been won and by whom.
+  //[1,2,3]  [4,5,6]  [7,8,9] [1,4,7] [2,5,8] [3,6,9] [1,5,9] [3,5,7]
+  checkWinningGame(){
+    console.log("in winning game");
+    console.log("------")
+    let a1 = this.gameBoard[0]
+    let b2 = this.gameBoard[1]
+    let c3 = this.gameBoard[2]
+    let d4 = this.gameBoard[3]
+    let e5 = this.gameBoard[4]
+    let f6 = this.gameBoard[5]
+    let g7 = this.gameBoard[6]
+    let h8 = this.gameBoard[7]
+    let i9 = this.gameBoard[8]
+    console.log("i9 is", i9)
+    console.log("------")
+    if ((a1 === "X" && b2 === "X" && c3 === "X")||
+        (d4 === "X" && e5 === "X" && f6 === "X")||
+        (g7 === "X" && h8 === "X" && i9 === "X")||
+        (a1 === "X" && d4 === "X" && g7 === "X")||
+        (b2 === "X" && e5 === "X" && h8 === "X")||
+        (c3 === "X" && f6 === "X" && i9 === "X")||
+        (a1 === "X" && e5 === "X" && i9 === "X")||
+        (c3 === "X" && e5 === "X" && g7 === "X")){
+          let game ={ board: [a1, b2, c3, d4, e5, f6, g7, h8, i9],
+                      player: 'Player 1'}
+          this.endGame(game)
+          return "Player 1"
+    }else if((a1 === "O" && b2 === "O" && c3 === "O")||
+        (d4 === "O" && e5 === "O" && f6 === "O")||
+        (g7 === "O" && h8 === "O" && i9 === "O")||
+        (a1 === "O" && d4 === "O" && g7 === "O")||
+        (b2 === "O" && e5 === "O" && h8 === "O")||
+        (c3 === "O" && f6 === "O" && i9 === "O")||
+        (a1 === "O" && e5 === "O" && i9 === "O")||
+        (c3 === "O" && e5 === "O" && g7 === "O")){
+          let game ={ board: [a1, b2, c3, d4, e5, f6, g7, h8, i9],
+                      player: 'Player 2'}
+          this.endGame(game)
+          return "Player 2"
+    }
+  }
+
+  endGame(game){
+      this.won = true
+      console.log(JSON.stringify(game))
+      //resetBoardView()
+      alert(`${game.player} wins! Taco`)
+      //fetch create new game in database
+    }
+
+    resetBoard(){
+      this.won = false
+      gameBoard.p1Turn = true
+      gameBoard.gameBoard = ["","","","","","","","",""]
+    }
+
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
   const board = document.getElementById('board')
-  board.addEventListener("click", checkState)
-  let gameBoard = ["","","","","","","","",""]
+  board.addEventListener("click", play)
+  let gameBoard = new Board("X", "O")
 
-  //intialize the state of the board when the DOM Loads player1 always starts therefor set var for p1Turn to true
-  const player1 = "X"
-  const player2 = "O"
-  let p1Turn    = true
-  let won       = false
+  function play(event){
 
-  function drawBoard(aBoard){
+    let token = event.target.dataset.token
+
+    // gamePlay
+    if(gameBoard.p1Turn && token === ""){
+      event.target.dataset.token = "X"
+      gameBoard.gameBoard[event.target.dataset.id-1] = event.target.dataset.token
+      gameBoard.p1Turn = false
+      drawBoardView(gameBoard.gameBoard)
+    }else if (!gameBoard.p1Turn && token === ""){
+      event.target.dataset.token = "O"
+      gameBoard.gameBoard[event.target.dataset.id-1] = event.target.dataset.token
+      gameBoard.p1Turn = true
+      drawBoardView(gameBoard.gameBoard)
+    }
+  }
+
+  function drawBoardView(aBoard){
     aBoard.forEach(function(piece, index){
       console.log(piece);
       if (piece === "X"){
@@ -19,98 +31,19 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelectorAll(`[data-id='${index+1}']`)[0].children[0].style.display ="block"
       }
     })
-    if (!won){
-      checkWinningGame()
-  }
-
-  }
-
-  function checkState(event){
-    //get the text content of the Span
-    let token = event.target.dataset.token
-
-    // gamePlay
-    if(p1Turn && token === ""){
-      event.target.dataset.token = "X"
-      gameBoard[event.target.dataset.id-1] = event.target.dataset.token
-      p1Turn = false
-      console.log("gameboard is", gameBoard)
-      console.log("----")
-      drawBoard(gameBoard)
-    }else if (!p1Turn && token === ""){
-      event.target.dataset.token = "O"
-      gameBoard[event.target.dataset.id-1] = event.target.dataset.token
-      p1Turn = true
-      console.log("p2 check state")
-      console.log("----")
-      drawBoard(gameBoard)
-    }
-  }
-
-
-  //checks to see if a game has been won and by whom.
-  //[1,2,3]  [4,5,6]  [7,8,9] [1,4,7] [2,5,8] [3,6,9] [1,5,9] [3,5,7]
-  function checkWinningGame(){
-    console.log("in winning game");
-    console.log("------")
-    let a1 = gameBoard[0]
-    let b2 = gameBoard[1]
-    let c3 = gameBoard[2]
-    let d4 = gameBoard[3]
-    let e5 = gameBoard[4]
-    let f6 = gameBoard[5]
-    let g7 = gameBoard[6]
-    let h8 = gameBoard[7]
-    let i9 = gameBoard[8]
-    console.log("i9 is", i9)
-    console.log("------")
-    if ((a1 === "X" && b2 === "X" && c3 === "X")||
-        (d4 === "X" && e5 === "X" && f6 === "X")||
-        (g7 === "X" && h8 === "X" && i9 === "X")||
-        (a1 === "X" && d4 === "X" && g7 === "X")||
-        (b2 === "X" && e5 === "X" && h8 === "X")||
-        (c3 === "X" && f6 === "X" && i9 === "X")||
-        (a1 === "X" && e5 === "X" && i9 === "X")||
-        (c3 === "X" && e5 === "X" && g7 === "X")){
-          let game ={ board: [a1, b2, c3, d4, e5, f6, g7, h8, i9],
-                      player: 'Player 1'}
-          endGame(game)
-          return "Player 1"
-    }else if((a1 === "O" && b2 === "O" && c3 === "O")||
-        (d4 === "O" && e5 === "O" && f6 === "O")||
-        (g7 === "O" && h8 === "O" && i9 === "O")||
-        (a1 === "O" && d4 === "O" && g7 === "O")||
-        (b2 === "O" && e5 === "O" && h8 === "O")||
-        (c3 === "O" && f6 === "O" && i9 === "O")||
-        (a1 === "O" && e5 === "O" && i9 === "O")||
-        (c3 === "O" && e5 === "O" && g7 === "O")){
-          let game ={ board: [a1, b2, c3, d4, e5, f6, g7, h8, i9],
-                      player: 'Player 2'}
-          endGame(game)
-          return "Player 2"
-    }
-  }
-
-  function endGame(game){
-      won = true
-      //drawBoard(game.board)
-      console.log(JSON.stringify(game))
-      //resetBoard()
-      
-      alert(`${game.player} wins! Taco`)
-
-      //fetch create new game in database
+    if (!gameBoard.won){
+      gameBoard.checkWinningGame()
     }
 
-  function resetBoard(){
+  }
+
+  function resetBoardView(){
     Array.from(document.getElementsByClassName('interior-space')).forEach(function(interiorDivElements){
       interiorDivElements.dataset.token = ""
       interiorDivElements.children[0].style.display = "none"
       interiorDivElements.children[1].style.display = "none"
     })
-    won     = false
-    p1Turn  = true
-    gameBoard = ["","","","","","","","",""]
+    gameBoard.resetBoard()
   }
 
 });
